6f17ccd4b9f29ada6cb54e932974270074b0f975
Joel <joeldome17@gmail.com>
2022-06-12 17:50:27 -0500

    Integración de PHP con HTML

---
+++ integracion-php-con-html/1.php-con-html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>iJCode - PHP con HTML</title>
+</head>
+<body>
+  <h1>¡Soy h1 escrito con HTML!</h1>
+  <?php echo "<p>Soy un p escrito con PHP</p><br><br>"; ?>
+  <span>Soy un span de HTML</span>
+</body>
+</html>
