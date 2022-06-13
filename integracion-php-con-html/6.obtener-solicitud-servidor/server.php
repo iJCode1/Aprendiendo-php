5656b630362328a9b1cbc63dc35ca5630d997247
Joel <joeldome17@gmail.com>
2022-06-13 13:20:57 -0500

    Peticiones al servidor PHP

---
+++ integracion-php-con-html/6.obtener-solicitud-servidor/server.php
@@ -0,0 +1,12 @@
+<?php
+
+// Se imprimen los valores de las variables globales ($_GET, $_POST, $_REQUEST)
+
+// $_GET: Almacena los valores que se pasaron por parametro en la URL
+var_dump($_GET);
+
+// $_POST: Almacena los valores que fueron mandados por atras (POST)
+var_dump($_POST);
+
+// $_REQUEST: Almacena los valores tanto de $_GET y $_POST
+var_dump($_REQUEST);
