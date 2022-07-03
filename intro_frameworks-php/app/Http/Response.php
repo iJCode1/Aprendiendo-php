2430146f45388beea9db18611651f43f3179d35a
Joel <joeldome17@gmail.com>
2022-07-03 18:57:11 -0500

    Response

---
+++ intro_frameworks-php/app/Http/Response.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http;
+
+class Response{
+
+  protected $view;
+
+  public function __construct($view){
+    $this->view = $view;
+  }
+
+  public function getView(){
+    return $this->view;
+  }
+
+  public function send(){
+    $view = $this->getView();
+
+    $content = file_get_contents(__DIR__ . "/../../views/$view.php");
+    require __DIR__ . "/../../views/layout.php";
+  }
+
+}
+
+?>
